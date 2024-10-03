72e49520a15d242c731d932d67307c718a8071f3
Robin Weaver <strwtch13@gmail.com>
2024-10-03 15:33:02 -0700

    ~Created .tsx models for recipes and ingredients~

---
+++ react-web-app/src/models/ingredient.tsx
@@ -0,0 +1,13 @@
+export class Ingredient
+{
+    aisle!: string;
+    amount!: number;
+    id!: number;
+    image!: string;
+    measures!: any;
+    meta!: string[];
+    name!: string;
+    original!: string;
+    originalName!: string;
+    unit!: string;
+}
