94ee12655bd52e81e0e7da72bd5a8a69dc63e713
Roxy Abedi <roxy.abedi@gmail.com>
2024-10-04 11:52:01 -0700

    set up search sidebar

---
+++ react-web-app/src/components/recipes/filter-side-bar/FilterSideBar.tsx
@@ -0,0 +1,189 @@
+export default function FilterSideBar()
+{
+    return(
+        <aside>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        Equipment
+                    </label>
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        Cuisine
+                    </label>
+                </div>
+                <div className="form-check">
+                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                        Prep Time
+                    </label>
+                </div>
+
+                <div className="accordion" id="accordionExample">
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingOne">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                Diet
+                            </button>
+                        </h2>
+                        <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                            <div className="accordion-body">
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Gluten Free
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Ketogenic
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Vegetarian
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Lacto-Vegetarian
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Ovo-Vegetarian
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Vegan
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Pescetarian
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Paleo
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Primal
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Low FODMAP
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        WHOLE30
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingTwo">
+                            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Intolerances
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                            <div className="accordion-body">
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Dairy
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Egg
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Gluten
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Grain
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Peanut
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Seafood
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Sesame
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Shellfish
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Soy
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Sulfite
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Tree Nut
+                                    </label>
+                                </div>
+                                <div className="form-check">
+                                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Wheat
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </aside>
+    )
+}
--- react-web-app/src/components/recipes/search-recipe/SearchRecipe.tsx
+++ react-web-app/src/components/recipes/search-recipe/SearchRecipe.tsx
@@ -1,43 +1,54 @@
 import RecipeCard from "../recipe-card/RecipeCard";
-import {useState, useEffect} from "react";
-import spoonacularService from "../../../services/spoonacular-service";
+import { useState, useEffect } from "react";
+// import spoonacularService from "../../../services/spoonacular-service";
 import { SearchResults } from "../../../models/search-recipes/search-results";
+import './SearchRecipe.css';
+import FilterSideBar from "../filter-side-bar/FilterSideBar";
 
 export default function SearchRecipe() {
 
     const [recipes, setRecipes] = useState<SearchResults[]>([]);
 
-    async function loadRecipes()
-    {
-        const searchResults = await spoonacularService.getRecipesByUserInput("query=pasta&maxFat=25&number=2");
-        setRecipes(searchResults.results);
-    }
+    // async function loadRecipes()
+    // {
+    //     const searchResults = await spoonacularService.getRecipesByUserInput("query=pasta&maxFat=25&number=2");
+    //     setRecipes(searchResults.results);
+    // }
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        loadRecipes();
+    //     loadRecipes();
 
-    }, [])
+    // }, [])
 
 
     return (
-        <>
+        <div className="recipe-container container">
+            <FilterSideBar />
             <main>
-            {
-            recipes.map((recipe) => (
-                <RecipeCard key={recipe.id}
-                    title={recipe.title}
-                    image={recipe.image}
-                    ></RecipeCard>
-            ))
-            }
-            </main>
-            <aside>
                 <form className="d-flex">
                     <input className="form-control me-sm-2" type="search" placeholder="Search" />
                     <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
                 </form>
-            </aside>
-        </>
+
+                {
+                    recipes.map((recipe) => (
+                        <RecipeCard key={recipe.id}
+                            title={recipe.title}
+                            image={recipe.image}
+                        ></RecipeCard>
+                    ))
+                }
+                <RecipeCard
+                    title="title"
+                    image="image" />
+                <RecipeCard
+                    title="title"
+                    image="image" />
+                <RecipeCard
+                    title="title"
+                    image="image" />
+            </main>
+        </div>
     )
 }   
