a17de5e92ec763436a2a918e093634ff92a98aa7
Jane Huynh <janelehuynh1999@gmail.com>
2024-10-08 15:52:30 -0700

    button opens add recipe modal with form inputs

--- react-web-app/src/components/user-pages/personal-library/PersonalLibrary.tsx
+++ react-web-app/src/components/user-pages/personal-library/PersonalLibrary.tsx
@@ -4,8 +4,7 @@ import spoonacularService from "../../../services/spoonacular-service";
 import { useEffect, useState } from "react";
 import { LibraryRecipeCard } from "../../../models/personal-library/library-recipe-card";
 
-export default function PersonalLibrary()
-{
+export default function PersonalLibrary() {
     const firstName = 'Gregor';
     const lastName = 'Dzierzon';
 
@@ -17,10 +16,10 @@ export default function PersonalLibrary()
 
     }, [])
 
-    async function getLibrary()
-    {
+    async function getLibrary() {
         // internal API call
         const libraryItems = await recipesListService.getUserLibrary();
+
         setLibrary(libraryItems);
 
         // external API call
@@ -29,27 +28,65 @@ export default function PersonalLibrary()
 
     return (
         <>
-        <h3>{firstName} {lastName}'s Personal Library</h3>
+            <h3>{firstName} {lastName}'s Personal Library</h3>
 
-        <main className="container">
+            <main className="container">
 
-        <div>Preview Recipe Cards Here</div>
-        <button className="btn btn-info">Add Recipe</button>
-        <form className="d-flex" >
-            <input type="text" className="form-control me-sm-2" placeholder="Search" />
-            <button type="submit" className="btn btn-secondary my-2 my-sm-0">Search</button> 
-        </form>
+                <div>Preview Recipe Cards Here</div>
 
-        {
-            library.map((recipe) => (
-                    <RecipeCard key={recipe.id}
+                <button className="btn btn-info" data-bs-toggle="modal" data-bs-target="#exampleModal">Add Recipe</button>
+                <div className="modal fade" id="exampleModal" tabIndex={-1} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">Add New Recipe</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true"></span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div>
+                                        <label className="form-label" htmlFor="title">Title</label>
+                                        <input className="form-control border-primary" type="text" name="title" id="title" />
+                                    </div>
+                                    <div>
+                                        <label className="form-label" htmlFor="title">Image URL</label>
+                                        <input className="form-control border-warning" type="text" name="title" id="title" disabled />
+                                    </div>
+                                    <div>
+                                        <label className="form-label" htmlFor="title">Ingredients</label>
+                                        <textarea className="form-control border-primary" name="title" id="title" />
+                                    </div>
+                                    <div>
+                                        <label className="form-label" htmlFor="title">Instructions</label>
+                                        <textarea className="form-control border-primary" name="title" id="title" />
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-primary">Save Recipe</button>
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <form className="d-flex" >
+                    <input type="text" className="form-control me-sm-2" placeholder="Search" />
+                    <button type="submit" className="btn btn-secondary my-2 my-sm-0">Search</button>
+                </form>
+
+                {
+                    library.map((recipe) => (
+                        <RecipeCard key={recipe.id}
                             isCustom={recipe.isCustom}
                             title={recipe.isCustom ? recipe.customTitle : recipe.externalTitle}
                             image={recipe.isCustom ? recipe.customImage : recipe.externalImage}
                             id={recipe.isCustom ? recipe.customId : recipe.apiId} />
-            ))
-        }
-        </main>
+                    ))
+                }
+            </main>
         </>
     )
 }
